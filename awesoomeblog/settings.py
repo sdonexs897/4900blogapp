717ccff1f2636e6c2a845e8e1b72ad0941860272
sdone <sdone@unomaha.edu>
2021-11-10 16:51:57 -0600

    updates

--- awesoomeblog/settings.py
+++ awesoomeblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x@bon=4vmzan&ij))0kv^mrfu9zmgfjf7=c&ccb*gr3h7l)8e&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
